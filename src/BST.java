afd6068aedec3b7a514aa8117f9948f0be484c97
Jonathan Yee <jonathan.yee@ucalgary.ca>
2018-02-18 17:11:39 -0700

    Added BST traversal functions. They are not complete.

--- src/BST.java
+++ src/BST.java
@@ -1,8 +1,50 @@
+import java.util.Queue;
 
 public class BST {
 	private Node root;
 	
 	public BST() {
 		root = null;
+	}
+	public void insert(char opcd, int snum, String lnam, String dep, String prog, int yr) {
+		Node current = root, parent = null;
+		while(current != null) {
+			parent = current;
+			if(lnam.compareTo(current.getLastname()) > 0)
+				current = current.getRight();
+			else
+				current = current.getLeft();
+		}
+		if(root == null)
+			root = new Node(opcd, snum, lnam, dep, prog, yr, null, null, parent);
+		else if(lnam.compareTo(parent.getLastname())>0)
+			parent.setRight(new Node(opcd, snum, lnam, dep, prog, yr, null, null, parent));
+		else
+			parent.setLeft(new Node(opcd, snum, lnam, dep, prog, yr, null, null, parent));
+	}
+	/*
+	 * Recursively visiting all nodes: Depth-first.
+	 */
+	public void depthfirst(Node current) {
+		if(current != null) {
+			depthfirst(current.getLeft());
+		//do something at current
+			depthfirst(current.getRight());
+		}
+	}
+	public void breadthfirst() {
+		Node p = root;
+		Queue queue = new Queue();
+		if (p != null) {
+			queue.enqueue(p);
+			while (!queue.isEmpty()) {
+				p = (Node) queue.dequeue();
+				//do something at p
+				if (p.getLeft() != null)
+					queue.enqueue(p.getLeft());
+				if (p.getRight() != null)
+					queue.enqueue(p.getRight());
+			}
+		}
 	}
 }
--- src/Node.java
+++ src/Node.java
@@ -8,21 +8,79 @@ public class Node {
 	private String department;
 	private String program;
 	private int year;
-	private Node left, right;
+	private Node left, right, parent;
 	
 	public Node() {
-		left = right = null;
+		setParent(setLeft(setRight(null)));
 	}
 	public Node(char opcd, int snum, String lnam, String dep, String prog, int yr) {
-		this(opcd, snum, lnam, dep, prog, yr, null, null);
+		this(opcd, snum, lnam, dep, prog, yr, null, null, null);
 	}
-	public Node(char opcd, int snum, String lnam, String dep, String prog, int yr, Node lt, Node rt) {
-		opcode = opcd;
-		studentnum = snum;
-		lastname= lnam;
-		department = dep;
-		program = prog;
-		left = lt;
-		right = rt;
+	public Node(char opcd, int snum, String lnam, String dep, String prog, int yr, Node lt, Node rt, Node p) {
+		setOpcode(opcd);
+		setStudentnum(snum);
+		setLastname(lnam);
+		setDepartment(dep);
+		setProgram(prog);
+		setLeft(lt);
+		setRight(rt);
+		setParent(p);
 	}
+	public char getOpcode() {
+		return opcode;
+	}
+	public void setOpcode(char opcode) {
+		this.opcode = opcode;
+	}
+	public int getStudentnum() {
+		return studentnum;
+	}
+	public void setStudentnum(int studentnum) {
+		this.studentnum = studentnum;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getProgram() {
+		return program;
+	}
+	public void setProgram(String program) {
+		this.program = program;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public Node getLeft() {
+		return left;
+	}
+	public Node setLeft(Node left) {
+		this.left = left;
+		return left;
+	}
+	public Node getRight() {
+		return right;
+	}
+	public Node setRight(Node right) {
+		this.right = right;
+		return right;
+	}
+	public Node getParent() {
+		return parent;
+	}
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	
 }
